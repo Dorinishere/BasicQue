deb14de28a73238f6aed2e47b45862c15c5af44e
Dorin Bamniya <123871946+Dorinishere@users.noreply.github.com>
2024-04-19 16:54:52 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (58.20%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+        
+        int k = 2; // Initialize k to 2, as the first two elements are always unique
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k++] = nums[i]; // Move the unique element to the front
+            }
+        }
+        
+        return k;
+    }
+}
